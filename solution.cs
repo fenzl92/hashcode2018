978a6345a3f232670adc9656fca2b58c2c3437e9
DinkieShy <32346447+DinkieShy@users.noreply.github.com>
2018-03-01 18:11:14 +0000

    Added classes

---
+++ solution.cs
@@ -0,0 +1,20 @@
+//Hashcode 2018
+
+using System;
+
+class program{
+	static void Main(){
+		
+	}
+}
+
+class ride{
+	public int startX; //x coord of start pos
+	public int startY; //y coord of start pos
+	public int endX; //x coord of end pos
+	public int endY; //y coord of end pos
+}
+
+class car{
+	
+}
