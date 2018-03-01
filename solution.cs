b2bb4e3c6fceabf640f8f8c8916e7b108c8e0633
DinkieShy <32346447+DinkieShy@users.noreply.github.com>
2018-03-01 18:42:51 +0000

    Added Sim class

--- solution.cs
+++ solution.cs
@@ -11,8 +11,17 @@ class program{
 	static void readIn(){ //read data in from the .in file
 		string[] file = System.IO.ReadAllLines("a_example.in");
 		string[] currentLine = file[0].Split(' ');
-		
+		simulation Sim = new simulation();
+		Sim.availableCars = currentLine[2];
 	}
+}
+
+class simulation{
+	public int currentTime = 0; //current tick, initialised to 0
+	public int availableCars; //number of cars not on rides
+	public ride[] rides; //array of rides
+	public int bonus; //number of bonus points for being early
+	public int timeRemaining; //number of ticks left
 }
 
 class ride{
@@ -23,6 +32,8 @@ class ride{
 	
 	public int earliestStart; //earliest start time
 	public int latestFinish; //latest finish time
+	
+	public bool complete;
 }
 
 class car{
