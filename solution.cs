db8380b72357ecb4f14d2a50129c760b20f11d2e
DinkieShy <32346447+DinkieShy@users.noreply.github.com>
2018-03-01 18:26:21 +0000

    Started class variables

--- solution.cs
+++ solution.cs
@@ -1,9 +1,16 @@
 //Hashcode 2018
 
 using System;
+using System.IO;
 
 class program{
 	static void Main(){
+		
+	}
+	
+	static void readIn(){ //read data in from the .in file
+		string[] file = System.IO.ReadAllLines("a_example.in");
+		string[] currentLine = file[0].Split(' ');
 		
 	}
 }
@@ -13,8 +20,12 @@ class ride{
 	public int startY; //y coord of start pos
 	public int endX; //x coord of end pos
 	public int endY; //y coord of end pos
+	
+	public int earliestStart; //earliest start time
+	public int latestFinish; //latest finish time
 }
 
 class car{
-	
+	public int currentX; //current x coord
+	public int currentY; //current y coord
 }
